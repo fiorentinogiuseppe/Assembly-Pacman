83a2329badbfb947ce7f8abb641c1f3a8ae6d74e
Jonas Freire Alcantara <jonasfreirestudy@gmail.com>
2018-08-11 23:44:05 -0300

    iniciacao dos movimentos dos fantasmas

--- motion/motion.asm
+++ motion/motion.asm
@@ -303,6 +303,91 @@
 	# Pop from stack
 .end_macro
 
+
+#############################################################
+#############################################################
+#############################################################
+
+
+# Procedimento para mover o fantasma azul para esquerda
+# %blue_ghost_position	-> registrador que contem a posicao do no onde esta o fantasma azul
+# Ao final do procedimento o registrador %blue_ghost_position
+# contera o endereco para nova posicao do fantasma azul no grafo
+.macro move_blue_left (%blue_ghost_position)
+	# Push to stack
+	addi $sp, $sp, -24
+	sw $s0, 0($sp)
+	sw $s1, 4($sp)
+	sw $s2, 8($sp)
+	sw $s3, 12($sp)
+	sw %blue_ghost_position, 16($sp)
+	sw $s4, 20($sp)
+	# Push to stack
+	
+	add $s0, $zero, %blue_ghost_position	# <$s0> armazenara o endereco do no onde o fantasma azul esta
+	lw $s4, 0($s0)				# <$s4> armazenara o valor do no do fantasma azul
+	
+	lw $s1, 16($s0)				# 
+	add $s1, $s0, $s1			# <$s1> armazenara o endereco do no da esquerda
+	
+	lw $s3, 0($s1)				# <$s3> armazenara o valor do no da esquerda
+	# Verificar se o elemento a esquerda e uma parede
+	beq $s3, 0x4, move_blue_left_finalization	# Se o no da esquerda e parede fazer nada
+	
+	# Verificar se o elemento a esquerda e um pac dot
+	bne $s3, 0x1, move_blue_left_not_pac_dot	# Se o no da esquerda nao e um pac dot fazer nada
+	bne $s4, 0x51, move_blue_left_pac_dot_before	# Se o no ao qual o fantasma azul pertencia estava com pac dot adicionar valor de pac dot ao no
+	li $s3, 0x1
+	sw $s3, 0($s0)
+	li $s4, 0x00ffffff				#
+	lw $s3, 20($s0)					#
+	sw $s4, 0($s3)					# Pintar o pixel onde o fantasma azul estava para pac dot
+	j move_blue_left_pac_dot_not_before
+	move_blue_left_pac_dot_before:
+	sw $zero, 0($s0)				# Se o no ao qual o fantasma azul pertencia estava com pac dot adicionar valor de espaco vazio ao no
+	lw $s3, 20($s0)					#
+	sw $zero, 0($s3)				# Pintar o pixel onde o fantasma azul estava para pac dot
+	move_blue_left_pac_dot_not_before:
+	addi $s2, $zero, 0x51				#
+	sw $s2, 0($s1)					# Mudar o conteudo do no da esquerda para fantasma azul e pac dot
+	li $s2, 0x0000ffff
+	lw $s3, 20($s1)					#
+	sw $s2, 0($s3)					# Pintar o pixel a esquerda com o fantasma azul
+	sw $s1, 16($sp)					# Modificar a posicao do fantasma azul para o no da esquerda, quando for carregado da stack o valor sera o atualizado
+	j move_blue_left_finalization
+	move_blue_left_not_pac_dot:
+	
+	# Verificar se o elemento a esquerda e um espaco vazio
+	bne $s3, 0x0, move_blue_left_not_empty_space	# Se o no da esquerda nao e um espaco fazer nada
+	sw $zero, 0($s0)				# Mudar o conteudo do no anterior para espaco vazio
+	addi $s2, $zero, 0x5
+	sw $s2, 0($s1)					# Mudar o conteudo do no da esquerda para fantasma azul
+	lw $s3, 20($s0)
+	sw $zero, 0($s3)				# Pintar o pixel onde o fantasma azul estava para espaco vazio
+	li $s2, 0x0000ffff
+	lw $s3, 20($s1)					#
+	sw $s2, 0($s3)					# Pintar o pixel a esquerda com o fantasma azul
+	sw $s1, 16($sp)					# Modificar a posicao do fantasma azul para o no da esquerda, quando for carregado da stack o valor sera o atualizado
+	j move_blue_left_finalization
+	move_blue_left_not_empty_space:
+	
+		
+	move_blue_left_finalization:
+	
+	
+	# Pop from stack
+	lw $s0, 0($sp)
+	lw $s1, 4($sp)
+	lw $s2, 8($sp)
+	lw $s3, 12($sp)
+	lw %blue_ghost_position, 16($sp)
+	lw $s4, 20($sp)
+	addi $sp, $sp, 24
+	# Pop from stack
+.end_macro
+
+
+
 # TEST
 #.text
 #
